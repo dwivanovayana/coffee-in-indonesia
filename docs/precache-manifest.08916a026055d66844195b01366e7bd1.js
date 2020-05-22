b963d91d0df2ac6abc7c8c9b0728d1ee15e6d8d3
nova togatorop <nova@roadbeats.com>
2020-05-22 22:55:32 +0200

    Add build to docs

---
+++ docs/precache-manifest.08916a026055d66844195b01366e7bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e221fcc4848236b0263c6a4a5d73da5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a64d8c2371bec3d97dd",
+    "url": "/static/css/main.517aff5a.chunk.css"
+  },
+  {
+    "revision": "742b074942f12bbfff58",
+    "url": "/static/js/2.507a57e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.507a57e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a64d8c2371bec3d97dd",
+    "url": "/static/js/main.492d8966.chunk.js"
+  },
+  {
+    "revision": "5753bdc5c19df84e66f7",
+    "url": "/static/js/runtime-main.1ba30331.js"
+  }
+]);
